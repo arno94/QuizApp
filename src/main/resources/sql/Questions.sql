6f11acc4c1f2fb6d010fe18d346791d23897bce3
Boldizsár Erik <be1997deda@gmail.com>
2020-04-07 20:29:56 +0200

    Questions.sql file

---
+++ src/main/resources/sql/Questions.sql
@@ -0,0 +1,9 @@
+drop table if exists questions;
+
+create table Questions
+(
+    id int not null primary key,
+    question varchar(255) null,
+    answers varchar(255) null,
+    correct_answer varchar(255) null
+);
