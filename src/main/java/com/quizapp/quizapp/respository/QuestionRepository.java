196a021f24e2aa49167756680955892c3c1a7d72
Arno <arnpat01@gmail.com>
2020-05-03 18:13:32 +0200

    remove unused imports

--- src/main/java/com/quizapp/quizapp/respository/QuestionRepository.java
+++ src/main/java/com/quizapp/quizapp/respository/QuestionRepository.java
@@ -5,7 +5,6 @@ import com.quizapp.quizapp.enums.Difficulty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Set;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
